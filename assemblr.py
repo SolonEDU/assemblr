a40e641031387d35339b33097f793939b07754da
ericlau00 <ericlau41802@gmail.com>
2020-01-04 10:52:36 -0500

    merge

--- assemblr.py
+++ assemblr.py
@@ -12,7 +12,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "<h1 style='color:blue'>Hello There!</h1>";
+    return render_template(
+        "landing.html",
+        title= "Landing"
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
