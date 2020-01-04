f9c3821fd1e4758c39caefeaf8d53c874f579d00
ericlau00 <ericlau41802@gmail.com>
2020-01-04 11:03:55 -0500

    routes

--- assemblr.py
+++ assemblr.py
@@ -14,8 +14,72 @@ app = Flask(__name__)
 def root():
     return render_template(
         "landing.html",
-        title= "Landing"
     )
 
+@app.route("/login")
+def login():
+    return render_template(
+        "login.html",
+    )
+
+@app.route("/register")
+def register():
+    return render_template(
+        "register.html"
+    )
+
+@app.route("/home")
+def home():
+    return render_template(
+        "home.html"
+    )
+
+@app.route("/network")
+def network():
+    return render_template(
+        "network.html"
+    )
+
+@app.route("/my_people")
+def my_people():
+    return render_template(
+        "my_people.html"
+    )
+
+@app.route("other_people")
+def other_people():
+    return render_template(
+        "other_people.html"
+    )
+
+@app.route("new_project")
+def new_project():
+    return render_template(
+        "new_project.html"
+    )
+
+@app.route("new_team")
+def new_team():
+    return render_template(
+        "new_team.html"
+    )
+
+@app.route("profile")
+def profile():
+    return render_template(
+        "profile.html"
+    )
+
+@app.route("project")
+def project():
+    return render_template(
+        "project.html"
+    )
+
+@app.route("team")
+def team():
+    return render_template(
+        "team.html"
+    )
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
