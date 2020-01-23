230a214774d629b4f53c13361633301813fa822b
ericlau00 <ericlau41802@gmail.com>
2020-01-22 23:11:01 -0500

    more routing

--- assemblr.py
+++ assemblr.py
@@ -1,13 +1,13 @@
 import urllib.parse
 import urllib.request
 import os
-import sqlite3
+# import sqlite3
 import json
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from functools import wraps
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from utl import models
+# from flask_sqlalchemy import SQLAlchemy
+# from config import Config
+# from utl import models
 from google.cloud import firestore
 
 # db = models.db
@@ -27,8 +27,8 @@ app = Flask(__name__)
 
 # creates secret key for sessions
 app.secret_key = os.urandom(32)
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = f"{os.path.dirname(os.path.abspath(__file__))}/static/json/assemblr-28d528f0fb82.json"
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = f"{os.path.dirname(os.path.abspath(__file__))}/static/json/assemblr-28d528f0fb82.json"
 db = firestore.Client()
 
 api_file = f"{os.path.dirname(os.path.abspath(__file__))}/static/json/api.json"
@@ -43,6 +43,33 @@ GITHUB_OAUTH_ROUTE = 'https://github.com/login/oauth/authorize'
 GITHUB_OAUTH_REDIRECT = 'http://localhost:5000/callback'
 GITHUB_API_ROUTE = 'https://api.github.com/graphql'
 GITHUB_TOKEN_URL = 'https://github.com/login/oauth/access_token'
+
+
+def graphql_query(query):
+    req = urllib.request.Request(
+        GITHUB_API_ROUTE,
+        data=query,
+        headers={'Authorization': f"Bearer {session['access_token']}"},
+        method='POST'
+    )
+
+    req = urllib.request.urlopen(req)
+    res = req.read()
+
+    return json.loads(res)
+
+
+def connect_required(f):
+    '''
+    decorator that redirects user to landing page if not connected to github
+    '''
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if not 'access_token' in session:
+            flash("You must be connected to Github to view that page", "error")
+            return redirect(url_for('root'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 @app.route('/connect_to_github')
@@ -71,7 +98,7 @@ def callback():
 
     post_request = urllib.request.Request(
         GITHUB_TOKEN_URL,
-        headers= {'Accept' : 'application/json'},
+        headers={'Accept': 'application/json'},
         data=urllib.parse.urlencode(code_payload).encode()
     )
 
@@ -84,80 +111,32 @@ def callback():
     token_type = response_data['token_type']
 
     session['access_token'] = access_token
-    query = { 'query':
-    """
-    {
-        viewer {
-            name
-            login
-            topRepositories(first: 20, orderBy: {direction: ASC, field: UPDATED_AT}) {
-                edges {
-                    node {
-                        languages(first: 3, orderBy: {direction: ASC, field: SIZE}) {
-                            edges {
-                                node {
-                                    color
-                                    name
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-    """ }
+
+    query = {'query': '{ viewer { login name } }'}
     query = json.dumps(query).encode('utf8')
-    req = urllib.request.Request(
-        GITHUB_API_ROUTE,
-        data = query,
-        headers= {'Authorization': f"Bearer {session['access_token']}"},
-        method= 'POST'
-    )
 
-    req = urllib.request.urlopen(req)
-    res = req.read()
+    result = graphql_query(query)
 
-    session['login'] = json.loads(res)['data']['viewer']['login']
-    session['name'] = json.loads(res)['data']['viewer']['name']
+    session['login'] = result['data']['viewer']['login']
+    session['name'] = result['data']['viewer']['name']
 
-    # if session['login'] is in users table:
-    #     go to home page
-
-    # if session['login'] is not in users table:
-        # get languages and redirect to registration page
-
-    user_languages = dict()
-
-    for edge in json.loads(res)['data']['viewer']['topRepositories']['edges']:
-        if not edge is None:
-            for language in edge['node']['languages']['edges']:
-                user_languages[language['node']['name']] = language['node']['color']
-
-    session['user_languages'] = user_languages
-    return redirect(url_for('register'))
-
-def login_required(f):
-    '''
-    decorator that redirects user to login page if not logged in
-    '''
-    @wraps(f)
-    def decorated_function(*args, **kwargs):
-        if "uid" not in session:
-            flash("You must be logged in to view this page", "error")
-            return redirect(url_for('root'))
-        return f(*args, **kwargs)
-    return decorated_function
+    if db.collection('users').document(session['login']).get().exists:
+        return redirect(url_for('home'))
+    else:
+        return redirect(url_for('register'))
 
 
 @app.route("/")
 def root():
-    if "uid" in session:
-        return redirect(url_for('home'))
-    else:
-        return render_template(
-            "landing.html",
-        )
+    return redirect(url_for('home')) if 'login' in session else render_template('landing.html')
+
+
+@app.route('/home')
+@connect_required
+def home():
+    return render_template(
+        'home.html'
+    )
 
 
 @app.route('/about')
@@ -174,50 +153,46 @@ def help():
     )
 
 
-@app.route("/login")
-def login():
-    if "uid" in session:
-        return redirect(url_for('home'))
-    else:
-        return render_template(
-            "login.html",
-        )
-
-
-# @app.route("/auth", methods=["POST"])
-# def auth():
-#     '''
-#     authenticates user upon a login attempt
-#     '''
-#     if "uid" in session:
-#         flash("You were already logged in, "+session['email']+".", "error")
-#         return redirect(url_for('home'))
-#     # information inputted into the form by the user
-#     email = request.form['email']
-#     password = request.form['password']
-#     # looking for email & password from database
-#     user = User.query.filter_by(email=email).first()
-#     if user == None:  # if email not found
-#         flash("No user found with given email", "error")
-#         return redirect(url_for('login'))
-#     elif password != user.password:  # if password is incorrect
-#         flash("Incorrect password", "error")
-#         return redirect(url_for('login'))
-#     else:  # hooray! the email and password are both valid
-#         session['uid'] = user.uid
-#         session['email'] = user.email
-#         flash("Welcome " + email +
-#               ". You have been logged in successfully.", "success")
-#         return redirect(url_for('home'))
-
 @app.route('/register', methods=['GET', 'POST'])
+@connect_required
 def register():
+    if db.collection('users').document(session['login']).get().exists:
+        return redirect(url_for('home'))
     if request.method == 'GET':
+        query = {'query':
+                 '''
+        {
+            viewer {
+                topRepositories(first: 20, orderBy: {direction: ASC, field: UPDATED_AT}) {
+                    edges {
+                        node {
+                            languages(first: 3, orderBy: {direction: ASC, field: SIZE}) {
+                                edges { node { color name } }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        '''}
+        query = json.dumps(query).encode('utf8')
+
+        result = graphql_query(query)
+
+        languages = dict()
+
+        for edge in result['data']['viewer']['topRepositories']['edges']:
+            if not edge is None:
+                for language in edge['node']['languages']['edges']:
+                    languages[language['node']['name']
+                              ] = language['node']['color']
         return render_template(
-            'register.html'
+            'register.html',
+            languages=languages
         )
     else:
-        languages = {language : request.form[language] for language in request.form.getlist('language')}
+        languages = {language: request.form[language]
+                     for language in request.form.getlist('language')}
 
         doc_ref = db.collection('users').document(request.form['username'])
         doc_ref.set({
@@ -227,70 +202,26 @@ def register():
             'languages': languages
         })
 
-        return render_template(
-            'register.html'
-        )
-
-# @app.route("/signup")
-# def signup():
-#     if "uid" in session:
-#         return redirect(url_for('home'))
-#     else:
-#         return render_template(
-#             "register.html"
-#         )
+        return redirect(url_for('home'))
 
 
-# @app.route("/register", methods=["POST"])
-# def register():
-#     '''
-#     creates a new user in the database if provided valid signup information
-#     '''
-#     if "uid" in session:
-#         return redirect(url_for('home'))
-#     # gets user information from POST request
-#     firstname = request.form['firstname']
-#     lastname = request.form['lastname']
-#     email = request.form['email']
-#     github = request.form['github']
-#     password = request.form['password']
-#     password2 = request.form['password2']
-#     age = int(request.form['age'])
-#     city = request.form['city']
-#     bio = request.form['bio']
-#     # looking if user with email in database already exists
-#     user = User.query.filter_by(email=email).first()
-#     if user != None:
-#         flash("Account with that email already exists", "error")
-#         return redirect(url_for('signup'))
-#     elif password != password2:
-#         flash("Passwords do not match", "error")
-#         return redirect(url_for('signup'))
-#     elif len(password) < 8:
-#         flash("Password must be at least 8 characters in length", "error")
-#         return redirect(url_for('signup'))
-#     # successfully add user to database
-#     else:
-#         newuser = User(firstname=firstname, lastname=lastname, email=email,
-#                        github=github, password=password, age=age, city=city, bio=bio)
-#         db.session.add(newuser)
-#         db.session.commit()
-#         flash("Successfully created user", "success")
-#         return redirect(url_for('login'))
+@app.route('/profile')
+@connect_required
+def profile():
+    return render_template(
+        'profile.html'
+    )
 
 
-# @app.route("/logout")
-# def logout():
-#     '''
-#     logs out user by deleting info from the session
-#     '''
-#     if not "uid" in session:
-#         flash("Already logged out, no need to log out again", "error")
-#     else:
-#         session.pop('uid')
-#         session.pop('email')
-#         #flash("Successfully logged out", "success")
-#     return redirect(url_for('root'))  # should redirect back to login page
+@app.route('/logout')
+def logout():
+    '''
+    logs out user by deleting info from the session
+    '''
+    session.pop('login')
+    session.pop('name')
+    session.pop('access_token')
+    return redirect(url_for('root'))
 
 
 # @app.route("/home")
